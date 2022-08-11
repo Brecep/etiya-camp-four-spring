ab4b73fab824bc8b0ca69ca273e0969d004dde8d
Recep Bayındır (ETIYA) <recep.bayindir@etiya.com>
2022-08-11 18:02:29 +0300

    results added

--- northwind/northwind/src/main/java/com/etiya/northwind/business/abstracts/CustomerService.java
+++ northwind/northwind/src/main/java/com/etiya/northwind/business/abstracts/CustomerService.java
@@ -3,26 +3,30 @@ package com.etiya.northwind.business.abstracts;
 import java.util.List;
 import java.util.Map;
 
-
 import com.etiya.northwind.business.requests.customers.CreateRequestCustomer;
 import com.etiya.northwind.business.requests.customers.DeleteRequestCustomer;
 import com.etiya.northwind.business.requests.customers.UpdateRequestCustomer;
 import com.etiya.northwind.business.responses.customers.CustomerListResponse;
 import com.etiya.northwind.business.responses.customers.GetCustomerResponse;
+import com.etiya.northwind.core.utilities.results.DataResult;
+import com.etiya.northwind.core.utilities.results.Result;
 
 public interface CustomerService {
-	
-	List<CustomerListResponse> getAll();
-	void add(CreateRequestCustomer createRequestCustomer);
-	void update(UpdateRequestCustomer updateRequestCustomer);
-	
-	
-	void delete(DeleteRequestCustomer deleteRequestCustomer);
-	GetCustomerResponse getcustomerId(String customerId);
-	
-	List<CustomerListResponse> getAllPages(int pageNo , int pageSize);
-	 Map<String,Object>getAllPagesSort(int pageNo, int pageSize,String entity,String type);
-	
+
+	DataResult<List<CustomerListResponse>> getAll();
+
+	Result add(CreateRequestCustomer createRequestCustomer);
+
+	Result update(UpdateRequestCustomer updateRequestCustomer);
+
+	Result delete(DeleteRequestCustomer deleteRequestCustomer);
+
+	DataResult<GetCustomerResponse> getcustomerId(String customerId);
+
+	DataResult<List<CustomerListResponse>> getAllPages(int pageNo, int pageSize);
+
+	Map<String, Object> getAllPagesSort(int pageNo, int pageSize, String entity, String type);
+
 //	List<Customer> findCustomersWithSorting(String field);
 //	   Page<Customer> findCustomersWithPagination(int offset,int pageSize);
 //	   Page<Customer> findCustomersWithPaginationAndSorting(int offset,int pageSize,String field);
--- northwind/northwind/src/main/java/com/etiya/northwind/business/concretes/CustomerManager.java
+++ northwind/northwind/src/main/java/com/etiya/northwind/business/concretes/CustomerManager.java
@@ -18,6 +18,8 @@ import com.etiya.northwind.business.requests.customers.UpdateRequestCustomer;
 import com.etiya.northwind.business.responses.customers.CustomerListResponse;
 import com.etiya.northwind.business.responses.customers.GetCustomerResponse;
 import com.etiya.northwind.core.utilities.mapping.ModelMapperService;
+import com.etiya.northwind.core.utilities.results.DataResult;
+import com.etiya.northwind.core.utilities.results.SuccessDataResult;
 import com.etiya.northwind.dataAccess.abstracts.CustomerRepository;
 import com.etiya.northwind.entities.concretes.Customer;
 
@@ -28,36 +30,33 @@ public class CustomerManager implements CustomerService {
 	private ModelMapperService modelMapperService;
 
 	public CustomerManager(CustomerRepository customerRepository, ModelMapperService modelMapperService) {
-	
+
 		this.customerRepository = customerRepository;
 		this.modelMapperService = modelMapperService;
 	}
 
 	@Override
-	public List<CustomerListResponse> getAll() {
+	public DataResult<List<CustomerListResponse>> getAll() {
 		List<Customer> result = this.customerRepository.findAll();
-		List<CustomerListResponse> response = result.stream().map(
-				customer-> this.modelMapperService.forResponse().
-				map(customer, CustomerListResponse.class)).collect(Collectors.toList());
+		List<CustomerListResponse> response = result.stream()
+				.map(customer -> this.modelMapperService.forResponse().map(customer, CustomerListResponse.class))
+				.collect(Collectors.toList());
 
-		
-		
-		return response;
+		return new SuccessDataResult<List<CustomerListResponse>>(response);
 	}
 
 	@Override
 	public void add(CreateRequestCustomer createRequestCustomer) {
-		Customer customer = this.modelMapperService.forRequest().
-				map(createRequestCustomer, Customer.class);
+		Customer customer = this.modelMapperService.forRequest().map(createRequestCustomer, Customer.class);
 		this.customerRepository.save(customer);
-		
+
 	}
 
 	@Override
 	public void update(UpdateRequestCustomer updateRequestCustomer) {
 		Customer customer = this.modelMapperService.forRequest().map(updateRequestCustomer, Customer.class);
 		this.customerRepository.save(customer);
-		
+
 	}
 
 //	@Override
@@ -78,73 +77,58 @@ public class CustomerManager implements CustomerService {
 //        return  customers;
 //	}
 
-	
-	
-
-
 	@Override
 	public void delete(DeleteRequestCustomer deleteRequestCustomer) {
-		
-        this.customerRepository.deleteById(deleteRequestCustomer.getCustomerId());
+
+		this.customerRepository.deleteById(deleteRequestCustomer.getCustomerId());
 	}
 
 	@Override
 	public GetCustomerResponse getcustomerId(String customerId) {
-		   Customer customer = this.customerRepository.getById(customerId);
-	        GetCustomerResponse response = this.modelMapperService.forResponse().map(customer, GetCustomerResponse.class);
-	        return response;
+		Customer customer = this.customerRepository.getById(customerId);
+		GetCustomerResponse response = this.modelMapperService.forResponse().map(customer, GetCustomerResponse.class);
+		return response;
 	}
-	
-	
 
 	@Override
 	public List<CustomerListResponse> getAllPages(int pageNo, int pageSize) {
-		Pageable pageable = PageRequest.of(pageNo-1, pageSize);
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize);
 		List<Customer> customers = this.customerRepository.findAll(pageable).getContent();
-		List<CustomerListResponse> response = customers.stream().map(customer->
-		this.modelMapperService.forResponse().map(customer, CustomerListResponse.class)).collect(
-				Collectors.toList());
-		
+		List<CustomerListResponse> response = customers.stream()
+				.map(customer -> this.modelMapperService.forResponse().map(customer, CustomerListResponse.class))
+				.collect(Collectors.toList());
+
 		int currentPage = pageNo;
 		int totalDatas = customers.size();
-		int totalPages = (int) Math.ceil(totalDatas/pageSize);
-		
+		int totalPages = (int) Math.ceil(totalDatas / pageSize);
+
 		return response;
 	}
 
 	@Override
 	public Map<String, Object> getAllPagesSort(int pageNo, int pageSize, String entity, String type) {
-		
-		Pageable pageable=PageRequest.of(pageNo-1,pageSize,sortType(entity,type));
 
+		Pageable pageable = PageRequest.of(pageNo - 1, pageSize, sortType(entity, type));
 
-        Map<String,Object> response=new HashMap<>();
-        Page<Customer> customers =customerRepository.findAll(pageable);
-        response.put("totalElements",customers.getTotalElements()) ;
-        response.put("totalPages",customers.getTotalPages());
-        response.put("currentPage",customers.getNumber()+1);
-        response.put("customers",customers.getContent().stream().map(customer ->
-                this.modelMapperService.forResponse().map(customer,CustomerListResponse.class)).collect(Collectors.toList()));
+		Map<String, Object> response = new HashMap<>();
+		Page<Customer> customers = customerRepository.findAll(pageable);
+		response.put("totalElements", customers.getTotalElements());
+		response.put("totalPages", customers.getTotalPages());
+		response.put("currentPage", customers.getNumber() + 1);
+		response.put("customers",
+				customers.getContent().stream().map(
+						customer -> this.modelMapperService.forResponse().map(customer, CustomerListResponse.class))
+						.collect(Collectors.toList()));
 
-        return response ;
-    }
+		return response;
+	}
 
+	public Sort sortType(String property, String type) {
+		if (type.equals("desc"))
+			return Sort.by(property).descending();
+		else
+			return Sort.by(property).ascending();
 
- public Sort sortType(String property,String type){
-        if(type.equals("desc"))
-            return Sort.by(property).descending();
-        else return Sort.by(property).ascending() ;
-
-    }
-	
-
-
-
-
-	
-	
-	
-	
-	
+	}
 
 }
